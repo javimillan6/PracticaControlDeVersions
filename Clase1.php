34ce839965b511bb648523d11fb1c850a0d460ea
javimillan6 <millanjavi.4.1112@gmail.com>
2017-10-02 16:57:21 +0200

    Afegint metode1

---
+++ Clase1.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Clase1
+ *
+ * @author Javi
+ */
+class Clase1 {
+    function __construct() {
+                print "Hola soy el constructor";
+    }
+    
+    function metode1(){
+     
+    }
+}
